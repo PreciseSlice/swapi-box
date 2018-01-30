6cbfd74dc5fe265de9bf9ef7ee6ff773f8b786bc
Spencer Herms <slherms@gmail.com>
2018-01-30 15:32:36 -0700

    add normalize stylesheet to project, now rendering header component with a Favbutton

--- src/Components/FavButton/FavButton.jsx
+++ src/Components/FavButton/FavButton.jsx
@@ -1,2 +1,8 @@
-import React, { Component } from 'react';
-import '.FavButton.css';
+import React from 'react';
+import './FavButton.css';
+
+let FavCount = 0;
+
+const FavButton = (props) =>  <button>View Favorites {FavCount}</button>
+  
+export default FavButton;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,2 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Header.css';
+import FavButton from '../FavButton/FavButton.jsx'
+
+const Header = (props) => {
+  return (
+    <div className="header">
+      <h1>Swapi-box</h1>
+      <FavButton />
+    </div>
+  )
+}
+
+export default Header;
