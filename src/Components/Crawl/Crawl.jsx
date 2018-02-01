86b999548b093fc8d26e4a4209236750839d001e
Spencer Herms <slherms@gmail.com>
2018-01-31 23:31:14 -0700

    crawl renders data passed down from app awaits data to be recived before attempting render

--- src/Components/Crawl/Crawl.jsx
+++ src/Components/Crawl/Crawl.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Crawl.css';
+import PropTypes from 'prop-types';
 
 class Crawl extends Component {
   async componentDidMount() {
@@ -17,44 +18,87 @@ class Crawl extends Component {
       const title = film.title;
       const episodeId = film.episode_id;
       const openingCrawl = film.opening_crawl;
-      const releaseYear = film.release_date;
+      const releaseDate = film.release_date;
 
-      return { title, episodeId, openingCrawl, releaseYear };
+      return { title, episodeId, openingCrawl, releaseDate };
     });
     return Promise.all(filmMap);
   };
 
-  render() {
-    return (
-      <div className="crawl-container">
-        <div className="fade" />
-        <section className="star-wars">
-          <div className="crawl">
-            <div className="title">
-              <p>Episode IV</p>
-              <h1>A New Hope</h1>
-            </div>
+  romanize = num => {
+    if (!+num) return NaN;
+    let digits = String(+num).split(''),
+      key = [
+        '',
+        'C',
+        'CC',
+        'CCC',
+        'CD',
+        'D',
+        'DC',
+        'DCC',
+        'DCCC',
+        'CM',
+        '',
+        'X',
+        'XX',
+        'XXX',
+        'XL',
+        'L',
+        'LX',
+        'LXX',
+        'LXXX',
+        'XC',
+        '',
+        'I',
+        'II',
+        'III',
+        'IV',
+        'V',
+        'VI',
+        'VII',
+        'VIII',
+        'IX'
+      ],
+      roman = '',
+      index = 3;
+    while (index--) roman = (key[+digits.pop() + index * 10] || '') + roman;
+    return Array(+digits.join('') + 1).join('M') + roman;
+  };
 
-            <p>
-              It is a period of civil war. Rebel spaceships, striking from a
-              hidden base, have won their first victory against the evil
-              Galactic Empire.
-            </p>
-            <p>
-              During the battle, Rebel spies managed to steal secret plans to
-              the Empire’s ultimate weapon, the DEATH STAR, an armored space
-              station with enough power to destroy an entire planet.
-            </p>
-            <p>
-              Pursued by the Empire’s sinister agents, Princess Leia races home
-              aboard her starship, custodian of the stolen plans that can save
-              her people and restore freedom to the galaxy…
-            </p>
-          </div>
-        </section>
-      </div>
-    );
+  render() {
+    const { filmData } = this.props;
+    if (filmData.length) {
+      const randomFilm = filmData[Math.floor(Math.random() * filmData.length)];
+      return (
+        <div className="crawl-container">
+          <div className="fade" />
+          <section className="star-wars">
+            <div className="crawl">
+              <div className="title">
+                <p>{`Episode ${this.romanize(randomFilm.episodeId)}`}</p>
+                <h1>{randomFilm.title}</h1>
+              </div>
+              <p>{randomFilm.openingCrawl}</p>
+              <h3>{`Release Date: ${randomFilm.releaseDate}`}</h3>
+            </div>
+          </section>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
+
+Crawl.propTypes = {
+  filmData: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      episodeId: PropTypes.number.isRequired,
+      openingCrawl: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+};
 
 export default Crawl;
