128251accdb59741ec9ae6a3a8294af09c494be6
Spencer Herms <slherms@gmail.com>
2018-03-19 21:33:38 -0600

    move folder out of /components

---
+++ src/apiCalls/apiCaller.js
@@ -0,0 +1,116 @@
+export const callFetch = async url => {
+  try {
+    const getData = await fetch(url);
+    const jsonData = await getData.json();
+
+    return await jsonData;
+  } catch (er) {
+    const error = new Error('callFetch failed to fetch data');
+    return error;
+  }
+};
+
+export const getHomeworld = async url => {
+  try {
+    const homeworld = await callFetch(url);
+
+    return {
+      homeworld: homeworld.name,
+      population: homeworld.population
+    };
+  } catch (er) {
+    const error = new Error('getHomeworld failed to fetch data');
+    return error;
+  }
+};
+
+export const getName = async url => {
+  try {
+    const unresolvedPromises = url.map(async url => {
+      const eachName = await callFetch(url);
+
+      return eachName.name;
+    });
+    return Promise.all(unresolvedPromises);
+  } catch (er) {
+    const error = new Error('getName failed to fetch data');
+    return error;
+  }
+};
+
+export const getFilms = async url => {
+  try {
+    const fetchFilmData = await callFetch(url);
+
+    return {
+      title: fetchFilmData.title,
+      episodeId: fetchFilmData.episode_id,
+      openingCrawl: fetchFilmData.opening_crawl,
+      releaseDate: fetchFilmData.release_date
+    };
+  } catch (er) {
+    const error = new Error('getFilms failed to fetch filmData');
+    return error;
+  }
+};
+
+export const getPeople = async url => {
+  try {
+    const fetchPeopleData = await callFetch(url);
+
+    const peopleMap = fetchPeopleData.results.map(async person => {
+      const homeworldData = await getHomeworld(person.homeworld);
+      const speciesName = await getName(person.species);
+
+      return { name: person.name, ...homeworldData, species: speciesName };
+    });
+    return Promise.all(peopleMap);
+  } catch (er) {
+    const error = new Error('getPeople failed to fetch peopleData');
+    return error;
+  }
+};
+
+export const getPlanets = async url => {
+  try {
+    const fetchPlanetData = await callFetch(url);
+
+    const planetMap = fetchPlanetData.results.map(async planet => {
+      const getResidents = await getName(planet.residents);
+
+      const residentsReturn = await Promise.all(getResidents);
+      const residents = residentsReturn.join(', ');
+
+      return {
+        name: planet.name,
+        terrain: planet.terrain,
+        population: planet.population,
+        climate: planet.climate,
+        residents: residents
+      };
+    });
+    return Promise.all(planetMap);
+  } catch (er) {
+    const error = new Error('getPlanets failed to fetch planetData');
+    return error;
+  }
+};
+
+export const getVehicles = async url => {
+  try {
+    const fetchVehicleData = await callFetch(url);
+
+    const vehicleMap = fetchVehicleData.results.map(vehicle => {
+      return {
+        name: vehicle.name,
+        model: vehicle.model,
+        vehicleClass: vehicle.vehicle_class,
+        passengers: vehicle.passengers
+      };
+    });
+    return Promise.all(vehicleMap);
+  } catch (er) {
+    const error = new Error('getVehicles failed to fetch vehicleData');
+    return error;
+  }
+};
---
+++ src/apiCalls/apiCaller.test.js
@@ -0,0 +1,165 @@
+/* eslint-disable */
+import React from 'react';
+
+import {
+  callFetch,
+  getFilms,
+  getPeople,
+  getPlanets,
+  getVehicles,
+  getHomeworld,
+  getName
+} from './apiCaller';
+
+import {
+  allFilms,
+  cleanFilms,
+  allPeople,
+  cleanPeople,
+  allPlanets,
+  cleanPlanets,
+  allVehicles,
+  cleanVehicles,
+  urlArray,
+  residentsArray,
+  homeworldObject
+} from './apiMockData';
+
+describe('apiCaller', () => {
+  it('has multiple functions', () => {
+    expect(callFetch).toBeDefined();
+    expect(getFilms).toBeDefined();
+    expect(getPeople).toBeDefined();
+    expect(getPlanets).toBeDefined();
+    expect(getVehicles).toBeDefined();
+    expect(getHomeworld).toBeDefined();
+    expect(getName).toBeDefined();
+  });
+
+  describe('callFetch', () => {
+    it('returns data after making a fetch call', async () => {
+      const expectedParams = 'https://swapi.co/api/films/';
+
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(allFilms);
+          })
+      }));
+      callFetch(expectedParams);
+      expect(window.fetch).toHaveBeenCalledWith(expectedParams);
+    });
+
+    it('returns a data object if the server returns a status of ok', () => {
+      const url = 'https://swapi.co/api/films/';
+
+      expect(callFetch(url)).resolves.toEqual(allFilms);
+    });
+
+    it('throws an error if the data is not fetched ', async () => {
+      const url = 'https://swapi.co/api/films/';
+
+      window.fetch = jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          status: 500
+        })
+      );
+
+      expect(callFetch(url)).resolves.toEqual(
+        Error('callFetch failed to fetch data')
+      );
+    });
+  });
+
+  describe('helper functions', () => {
+    it('getHomeworld calls an endpoint and returns an object', async () => {
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(urlArray);
+          })
+      }));
+
+      const result = await getHomeworld(urlArray);
+
+      expect(result).toEqual(homeworldObject);
+    });
+
+    it('getName calls an endpoint and returns an array of names', async () => {
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(urlArray);
+          })
+      }));
+
+      const result = await getName(urlArray);
+
+      expect(result).toEqual(residentsArray);
+    });
+  });
+
+  describe('Cleaner functions', () => {
+    it('getFilms returns clean film data as an object', async () => {
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(allFilms);
+          })
+      }));
+
+      const url = 'https://swapi.co/api/films/';
+      const result = await getFilms(url);
+
+      expect(result).toEqual(cleanFilms);
+    });
+
+    it('GetPeople returns clean people data as an array of objects', async () => {
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(allPeople);
+          })
+      }));
+
+      const url = 'https://swapi.co/api/people/';
+      const result = await getPeople(url);
+
+      expect(result).toEqual(cleanPeople);
+    });
+
+    it('getPlanets returns clean planet data as an array of objects', async () => {
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(allPlanets);
+          })
+      }));
+
+      const url = 'https://swapi.co/api/planets/';
+      const result = await getPlanets(url);
+
+      expect(result).toEqual(cleanPlanets);
+    });
+
+    it('getVehicles returns clean vehicle data as an array of objects', async () => {
+      window.fetch = jest.fn().mockImplementation(() => ({
+        status: 200,
+        json: () =>
+          new Promise((resolve, reject) => {
+            resolve(allVehicles);
+          })
+      }));
+      const url = 'https://swapi.co/api/vehicles/';
+      const result = await getVehicles(url);
+
+      expect(result).toEqual(cleanVehicles);
+    });
+  });
+});
